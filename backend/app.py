d78ae3ad0a8cad53f3769ab6a673e2333f67e798
Mhp-yhyc <3280023917@qq.com>
2026-05-31 11:41:50 +0800

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -19,7 +19,7 @@ GITHUB_TOKEN = os.getenv("GH_TOKEN")
 REPO_OWNER = "Mhp-yhyc"
 REPO_NAME = "melsgix-iot-competiion" 
 GITHUB_BRANCH = "main"
-START_DIR = "tech-handbok/docs"
+START_DIR = "docs"
 SCAN_DEPTH = 2
 # =================================================================
 
